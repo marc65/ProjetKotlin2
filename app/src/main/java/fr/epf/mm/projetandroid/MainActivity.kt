202baa33ff3d2e5653f804f53c12547750e39e51
marc65 <lyffy962@gmail.com>
2023-06-07 10:21:20 +0200

    favorit almost there

---
+++ app/src/main/java/fr/epf/mm/projetandroid/MainActivity.kt
@@ -0,0 +1,113 @@
+package fr.epf.mm.projetandroid
+//import DetailsFilmActivity
+import MovieAdapter
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import fr.epf.mm.projetandroid.ApiManager
+import fr.epf.mm.projetandroid.Movie
+import fr.epf.mm.projetandroid.R
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var editTextSearch: EditText
+    private lateinit var buttonSearch: Button
+    private var movies: List<Movie> = emptyList()
+    private val handler = Handler(Looper.getMainLooper())
+    private var favoriteMovies: MutableList<Movie> = mutableListOf()
+    private lateinit var adapter: MovieAdapter
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        editTextSearch = findViewById(R.id.editTextSearch)
+        buttonSearch = findViewById(R.id.buttonSearch)
+
+        buttonSearch.setOnClickListener {
+            val query = editTextSearch.text.toString()
+            searchMovies(query)
+        }
+    }
+
+    private fun searchMovies(query: String) {
+        val apiKey = "0b96ae7ab4d6f3ff74468ec58e787def" // Votre clé API TMDB
+        val service = ApiManager.tmdbApiService
+
+        GlobalScope.launch(Dispatchers.IO) {
+            try {
+                val response = service.searchMovies(apiKey, query)
+                if (response.isSuccessful) {
+                    val searchResult = response.body()
+                    val movies = searchResult?.results
+                    if (movies != null) {
+                        withContext(Dispatchers.Main) {
+                            showMovies(movies)
+                        }
+                    } else {
+                        // Gérer le cas où la liste de films est null
+                    }
+                } else {
+                    // Gérer les erreurs de la requête
+                    val errorMessage = response.message()
+                    // Afficher ou traiter l'erreur
+                }
+            } catch (e: Exception) {
+                // Gérer les exceptions
+                e.printStackTrace()
+            }
+        }
+    }
+
+    private fun showMovies(movies: List<Movie>) {
+        this.movies = movies
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        adapter = MovieAdapter(movies)
+        recyclerView.adapter = adapter
+
+        adapter.setOnItemClickListener { movie ->
+            val intent = Intent(this@MainActivity, DetailsFilmActivity::class.java)
+            intent.putExtra("movie_id", movie.id)
+            intent.putExtra("favorite_movies", ArrayList(movies.filter { it.isFavorite }))
+            startActivity(intent)
+        }
+    }
+
+    fun showFavorites(view: View) {
+        //val favoriteMovies = movies.filter { it.isFavorite }
+        val intent = Intent(this, FavoriteActivity::class.java)
+        intent.putExtra("favorite_movies", ArrayList(favoriteMovies))
+        startActivity(intent)
+        //val favoriteMovies = intent.getSerializableExtra("favorite_movies") as? ArrayList<Movie>
+        //val intent = Intent(this, FavoriteActivity::class.java)
+        //intent.getSerializableExtra("favorite_movies") as? ArrayList<Movie>
+        //startActivity(intent)
+    }
+
+    fun updateFavoriteMovies() {
+        favoriteMovies.clear()
+        favoriteMovies.addAll(movies.filter { it.isFavorite })
+
+        // Mettre à jour la liste des films favoris dans le thread principal
+        handler.post {
+            adapter.notifyDataSetChanged()
+        }
+    }
+}
+
+
+
